788e5e728dcfc3282572fb78d93a33b852ee8256
Simon Levy <simon.levy01@post.runi.ac.il>
2024-11-06 16:45:24 +0200

    fixed Bill3 assignment to be more specific

--- Bill3.java
+++ Bill3.java
@@ -5,7 +5,7 @@ public class Bill3 {
 	    String name1 = args[0];
 	    String name2 = args[1];
 		String name3 = args[2];
-		int price = (int) Math.ceil((double)Integer.parseInt(args[3])/3); 
-		System.out.println("Dear " + name1 + ", " + name2 + ", " + name3 + ": pay " + price + " Shekels each");
+		double price = Math.ceil((double)Integer.parseInt(args[3])/3); 
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + price + " Shekels each");
 	}
 }
