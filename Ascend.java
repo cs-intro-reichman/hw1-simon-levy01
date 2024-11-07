4e7771f21ea953ad7bc732b1557d793306393c58
Simon Levy <simon.levy01@post.runi.ac.il>
2024-11-07 12:24:05 +0200

    finished assignment 4

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,17 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int a = ((int) (Math.random()*Integer.parseInt(args[0])));
+		int b = ((int) (Math.random()*Integer.parseInt(args[0])));
+		int c = ((int) (Math.random()*Integer.parseInt(args[0])));
+		int big = Math.max(Math.max(a,b), c); // find biggest integer
+		int small = Math.min(Math.min(a,b), c); // find smallest integer
+		// find middle integer by finding the 2 biggest numbers, then the smallest of those
+		int i = Math.max(a,b);
+		int j = Math.max(b,c);
+		int k = Math.max(c,a);
+		int mid = Math.min(Math.min(i,j),k);
+		System.out.println(a + " " + b + " " + c);
+		System.out.println(small + " " + mid + " " + big);
 	}
 }
